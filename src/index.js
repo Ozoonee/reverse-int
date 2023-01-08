1bbf98fe3f29cb309003afba0830a3c9953ee765
Alexander <95187064+Ozoonee@users.noreply.github.com>
2023-01-08 15:21:03 +0300

    feat: add reverse function code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if(typeof n !== 'number'){
+        return console.log('argument is not a number');
+    }
+    else if(String(n).length===1){
+        return console.log('enter number over +-9');
+    }
+    else{ 
+    if(n<0){n = n*-1}
+    numToStr = String(n)
+    console.log(result = +(numToStr.split('').reverse().join('')))
+    return result = +(numToStr.split('').reverse().join(''))}
 }
